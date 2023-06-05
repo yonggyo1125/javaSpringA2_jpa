9b39e02e8d3e80af438ab31c356d6b4965f232f3
yonggyo1125 <yonggyo00@kakao.com>
2023-06-05 14:22:26 +0900

    강의소스

--- jpa_study/board/src/main/java/org/koreait/controllers/member/MemberController.java
+++ jpa_study/board/src/main/java/org/koreait/controllers/member/MemberController.java
@@ -37,4 +37,9 @@ public class MemberController {
 
         return "redirect:/member/login";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "member/login";
+    }
 }
