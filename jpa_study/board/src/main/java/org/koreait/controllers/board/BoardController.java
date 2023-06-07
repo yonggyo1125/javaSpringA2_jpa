5f07c2b1d447f767e5d0f37393ceeab4bec4ac8b
yonggyo1125 <yonggyo00@kakao.com>
2023-06-07 15:02:41 +0900

    강의소스

--- jpa_study/board/src/main/java/org/koreait/controllers/board/BoardController.java
+++ jpa_study/board/src/main/java/org/koreait/controllers/board/BoardController.java
@@ -2,7 +2,6 @@ package org.koreait.controllers.board;
 
 import lombok.RequiredArgsConstructor;
 import org.koreait.entities.BoardData;
-import org.koreait.entities.Member;
 import org.koreait.models.board.BoardListService;
 import org.koreait.repositories.BoardDataRepository;
 import org.koreait.repositories.MemberRepository;
@@ -26,8 +25,13 @@ public class BoardController {
     @GetMapping
     @ResponseBody
     public void index() {
-        List<BoardData> items = listService.gets();
-        items.stream().forEach(System.out::println);
+
+        BoardData boardData = boardDataRepository.findById(1L).orElse(null);
+        boardData.setSubject("(수정)제목1");
+        boardDataRepository.flush();
+        //insertData();
+        //List<BoardData> items = listService.gets();
+        //items.stream().forEach(System.out::println);
         //List<BoardData> items = boardDataRepository.getBoardDatas();
 
         //insertData();
@@ -42,6 +46,7 @@ public class BoardController {
     }
 
     private void insertData() {
+        /**
         Member member = Member.builder()
                 .userId("user01")
                 .userNm("사용자01")
@@ -51,14 +56,14 @@ public class BoardController {
                 .build();
 
         member = memberRepository.saveAndFlush(member);
-
+        */
         List<BoardData> items = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             BoardData item = BoardData.builder()
                     .subject("제목" + i)
                     .content("내용" + i)
                     .poster("작성자" + i)
-                    .member(member)
+                    //.member(member)
                     .build();
             items.add(item);
         }
